0c95d4c87d7d4bdebe14ff710f2376bf191c6cfe
oZerro <leha.normatov@mail.ru>
2023-08-06 12:50:36 +0300

    Update db_hack

--- db_hack.py
+++ db_hack.py
@@ -1,8 +1,13 @@
+import random
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+
+recommendations = [
+    'Хвалю!', 'Молодец!', 'Отличная работа!', 'Справился лучше всех!'
+]
+
 def fix_marks(schoolkid):
-    schoolkid_bad_marks = Mark.objects.filter(schoolkid=schoolkid.id, points__in=[2,3])
-    for mark in schoolkid_bad_marks:
-        mark.points = 5
-        mark.save()
+    Mark.objects.filter(schoolkid=schoolkid.id, points__in=[2,3]).update(points=5)
 
 
 def remove_chastisements(schoolkid):
@@ -13,18 +18,24 @@ def remove_chastisements(schoolkid):
 def create_commendation(name, subject):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=name)
-    except Exception as ex:
+    except (ObjectDoesNotExist, MultipleObjectsReturned) as ex:
         print("Укажите имя и фамилию или полное ФИО")
-    subject = Subject.objects.get(title=lesson, year_of_study=schoolkid.year_of_study)
+        
+    subject = Subject.objects.get(title=subject, year_of_study=schoolkid.year_of_study)
+    
     lesson = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
         group_letter=schoolkid.group_letter, 
         subject=subject.id).first()
-    ivan_comment = Commendation(
-        text="Хвалю!",
-        created=lesson.date, 
-        schoolkid=schoolkid, 
-        subject=subject, 
-        teacher=lesson.teacher
-        )
-    ivan_comment.save()
+    
+    if lesson:
+        ivan_recommendation = Commendation.objects.create(
+            text=random.choice(recommendations),
+            created=lesson.date, 
+            schoolkid=schoolkid, 
+            subject=subject, 
+            teacher=lesson.teacher
+            )
+        
+
+    
